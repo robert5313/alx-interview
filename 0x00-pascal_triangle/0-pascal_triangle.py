344b908fc3f0f180a064137a2068e4c26a58477c
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-07-30 23:51:47 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-Pascal's Triangle
+0. Pascal's Triangle
 """
+
 
 def pascal_triangle(n):
     """
-    Create a function for def pascal_triangle(n):
+    A function called "pascal_triangle(n)" that will return a list of lists.
     """
     triangle = []
-    for i in range(1, n + 1):
-        level = []
-        K = 1
-        for j in range(1, i + 1):
-            level.append(K)
-            K = K * (i - j)
-        triangle.append(level)
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            triangle.append(level)
     return triangle
