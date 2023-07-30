623b878ef8521b343dc862659d9b7b792d3842e8
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-07-30 23:23:28 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,30 +1,13 @@
-def printPascal(n:int):
-  
- 
-    arr = [[0 for x in range(n)] 
-              for y in range(n)] 
-  
- 
-    for line in range (0, n):
-  
-        # Every line has number of 
-        # integers equal to line number
-        for i in range (0, line + 1):
-  
-            # First and last values 
-            # in every row are 1
-            if(i is 0 or i is line):
-                arr[line][i] = 1
-                print(arr[line][i], end = " ") 
-  
-            # Other values are sum of values
-            # just above and left of above 
-            else:
-                arr[line][i] = (arr[line - 1][i - 1] + 
-                                arr[line - 1][i])
-                print(arr[line][i], end = " ")             
-        print("\n", end = "")
-  
-# Driver Code
-n = 5
-printPascal(n)
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            last_row = triangle[-1]
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j+1])
+            row.append(1)
+        triangle.append(row)
+    return triangle
