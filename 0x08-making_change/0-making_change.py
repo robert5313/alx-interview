eff7c62f69d7064cd768f91b67d2a9a512504dfc
robert5313 <robert.kimaiyo@masterclass.co.ke>
2023-11-25 07:40:20 +0000

    makechange files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for j in range(coin, total + 1):
+            dp[j] = min(dp[j], dp[j - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
